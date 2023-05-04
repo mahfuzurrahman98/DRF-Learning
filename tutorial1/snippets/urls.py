7beb3eb797e1ea47ea7f112f8056cce841f51926
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-05-04 17:06:58 +0600

    some issues remain

--- tutorial1/snippets/urls.py
+++ tutorial1/snippets/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.snippet_detail, name='snippet-list'),
+    path('snippets/<int:pk>/', views.snippet_detail, name='snippet-detail'),
 ]
--- tutorial1/snippets/views.py
+++ tutorial1/snippets/views.py
@@ -7,30 +7,36 @@ from snippets.serializers import SnippetSerializer
 
 
 @csrf_exempt
-def snippet_list(request, pk=None):
+def snippet_detail(request, pk=None):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
-        if pk:
-            snippet = Snippet.objects.get(pk=pk)
-            if not snippet:
-                return JsonResponse(serializer.errors, status=400)
-            
-            serializer = SnippetSerializer(snippet)
-            return JsonResponse(serializer.data, safe=False, status=200)
-        else:
-            snippets = Snippet.objects.all()
-            serializer = SnippetSerializer(snippets, many=True)
-            return JsonResponse(serializer.data, safe=False, status=200)
+        try:
+            if pk:
+                snippet = Snippet.objects.get(pk=pk)
+                serializer = SnippetSerializer(snippet)
+                return JsonResponse(serializer.data, safe=False, status=200)
+            else:
+                snippets = Snippet.objects.all()
+                serializer = SnippetSerializer(snippets, many=True)
+                return JsonResponse(serializer.data, safe=False, status=200)
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status=400)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+        try:
+            data = JSONParser().parse(request)
+            if data is None:
+                return JsonResponse({"message": "no data in the request"}, status=400)
+            serializer = SnippetSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.errors, status=422)
+
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status=400)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
@@ -44,10 +50,13 @@ def snippet_list(request, pk=None):
             serializer.save()
             return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
-    
+
     elif request.method == 'DELETE':
+        # return a simple message
+        # return JsonResponse({"message": "delete method not implemented", "id": pk }, status=400)
+
         snippet = Snippet.objects.get(pk=pk)
         if not snippet:
-            return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.errors, status=422)
         snippet.delete()
         return HttpResponse(status=204)
