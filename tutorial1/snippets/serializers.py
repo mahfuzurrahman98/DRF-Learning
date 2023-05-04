9b478ab442cd46c2a9a537421316317436417960
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-05-04 15:10:24 +0600

    model-serializer introduced

--- tutorial1/snippets/serializers.py
+++ tutorial1/snippets/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
     # id = serializers.IntegerField(read_only=True)
     # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     # code = serializers.CharField(style={'base_template': 'textarea.html'})
